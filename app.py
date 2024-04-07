a8fc366780d89b2b1867d3752396ed8321e18e09
Eric Freitas <ericdennisfc@gmail.com>
2024-04-06 23:01:43 -0300

    criando dicionario

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 
-restaurantes = ['Pizza', 'Sushi']
+restaurantes = [{'nome':'Praça', 'categoria':'Japonesa', 'ativo':False},
+                {'nome':'Pizza Suprema', 'categoria':'Italiana', 'ativo':True},
+                {'nome':'Cantina', 'categoria':'Italiana', 'ativo':False}]
 
 def exibir_nome_do_programa():
     print ("""
@@ -45,7 +47,10 @@ def cadastrar_novo_restaurante():
 def listar_restaurantes():
     exibir_subtitulo('Listando restaurantes')
     for restaurante in restaurantes:
-        print(f'.{restaurante}')   
+        nome_restaurante = restaurante['nome']
+        categoria = restaurante['categoria']
+        ativo = restaurante['ativo']
+        print(f'- {nome_restaurante} / {categoria} / {ativo}')      
     voltar_ao_menu_principal()
 
 
